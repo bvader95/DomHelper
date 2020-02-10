63a920eebcf912e4816839e741cf08664a5c1ed1
Bartlomiej W <B.I.Weclewski@gmail.com>
2020-02-10 22:30:39 +0100

    Showing the equation to solve

--- app/src/main/java/pl/bweclewski/domhelper/MainActivity.java
+++ app/src/main/java/pl/bweclewski/domhelper/MainActivity.java
@@ -2,13 +2,29 @@ package pl.bweclewski.domhelper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 public class MainActivity extends AppCompatActivity {
   
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    generateProblem();
+  }
+  
+  @SuppressLint("SetTextI18n")
+  private void generateProblem() {
+    int term1;
+    int term2;
+    term1 = ThreadLocalRandom.current().nextInt(1,10);
+    term2 = ThreadLocalRandom.current().nextInt(1,10);
+    TextView equation = findViewById(R.id.equationTextView);
+    equation.setText(term1 + "+" + term2);
   }
 }
